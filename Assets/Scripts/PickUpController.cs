349ceeb40b93cc6524cf43b659af9f49f5773f87
Furkan Yıldız <furkanyld0@hotmail.com>
2025-10-30 17:39:33 +0300

    Fixed M249 animations

--- Assets/Scripts/PickUpController.cs
+++ Assets/Scripts/PickUpController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,11 @@ using UnityEngine;
 public class PickUpController : MonoBehaviour //bu script sadece weaponholder nesnesine atalý
 {
     private Gun currentGun;
+    private Player player;
     void Start()
     {
+        player = Player.Instance;
+
         Gun[] guns = FindObjectsByType<Gun>(FindObjectsSortMode.None);
         foreach(Gun g in guns) {
             DisableGun(g);
@@ -19,21 +23,32 @@ public class PickUpController : MonoBehaviour //bu script sadece weaponholder ne
 
     public void PickUpGun(Gun gun) {
         DropCurrentGun();
-
         currentGun = gun;
         currentGun.transform.SetParent(transform);
         currentGun.transform.localPosition = Vector3.zero;
         currentGun.transform.localRotation = Quaternion.Euler(Vector3.zero);
-        currentGun.transform.localScale = Vector3.one;
+        currentGun.transform.localScale = Vector3.one; //tekrar parente atýnca scale'i kendi kendine deðiþiyor
         EnableGun(currentGun);
     }
 
-    private void DropCurrentGun() {
+    public void DropCurrentGun() {
         DisableGun(currentGun);
         currentGun.transform.SetParent(null);
-        currentGun.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        currentGun.GetComponent<Rigidbody>().AddExplosionForce(5f ,transform.forward,1f);
-        
+
+        var currentGunRb = currentGun.GetComponent<Rigidbody>();
+        float dropForwardForce = 2f;
+        float dropUpwardForce = 2f;
+
+        currentGunRb.velocity = player.GetComponent<CharacterController>().velocity;
+
+        currentGunRb.AddForce(player.transform.forward * dropForwardForce, ForceMode.Impulse);
+        currentGunRb.AddForce(player.transform.up * dropUpwardForce, ForceMode.Impulse);
+
+        float random = UnityEngine.Random.Range(-1f,1f);
+        currentGunRb.AddTorque(new Vector3(random, random, random) * 10);
+
+
+
     }
 
     private void DisableGun(Gun gun) {
@@ -47,7 +62,7 @@ public class PickUpController : MonoBehaviour //bu script sadece weaponholder ne
     private void EnableGun(Gun gun) {
         gun.enabled = true;
         gun.GetComponent<GunAnimator>().enabled = true; //animator scriptini kapatýyoruz direkt
-        //gun.GetComponent<Animator>().enabled = true;
+        gun.GetComponent<Animator>().enabled = true;
         gun.GetComponent<CapsuleCollider>().enabled = false;
         gun.GetComponent<Rigidbody>().isKinematic = true;
         gun.GetComponent<Rigidbody>().useGravity = false;
